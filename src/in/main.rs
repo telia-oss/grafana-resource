e473101ccfa080c7a2b0a6c78b6120e0a4a5f126
Mohamad al moualem <m.almouallem@gmail.com>
2019-06-06 12:01:16 +0200

    Chore: remove extra code

--- src/in/main.rs
+++ src/in/main.rs
@@ -5,23 +5,9 @@ extern crate serde_json;
 
 use serde::{Deserialize, Serialize};
 
-use std::error::Error;
-use std::io::Read;
-
 #[derive(Serialize, Deserialize, Debug)]
 struct Version {
     pub r#ref: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct Source {
-    pub grafana_url: String,
-    pub grafana_token: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct InInput {
-    pub source: Source,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -29,27 +15,7 @@ struct InOutput {
     pub version: Version,
 }
 
-fn read_in_input() -> Result<InInput, Box<Error>> {
-    // Read from stdin
-    let mut input_buffer = String::new();
-    let stdin = std::io::stdin();
-    let mut handle = stdin.lock();
-    handle.read_to_string(&mut input_buffer)?;
-    // Deserialize
-    let input: InInput = serde_json::from_str(&input_buffer)?;
-
-    Ok(input)
-}
-
 fn main() {
-    let in_input = read_in_input();
-    if in_input.is_err() {
-        eprintln!(
-            "Error getting resource inInput! {:?}",
-            in_input.unwrap_err()
-        );
-        return;
-    }
     let ver = Version {
         r#ref: "ref".to_owned(),
     };
